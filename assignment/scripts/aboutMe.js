19fa1a56b8f14008d88e4c9860bc0f4f12577982
Daniel Richter <drichter3697@gmail.com>
2024-06-16 14:14:14 -0500

    finished 2.1 up until stretch goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,12 +1,12 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = 'Daniel ';
+let firstName = 'Daniel';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Richter';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-let fullName = firstName  +  lastName;
+let fullName = firstName + ' ' +  lastName;
 console.log('My name is '+ fullName);
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
@@ -32,7 +32,7 @@ console.log(food);
 let pets = 10
 console.log(pets);
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 20
+let friendsPets = 2
 // 10 - Add two pets to your `pets` variable
 pets += 2;
 console.log(pets);
@@ -42,13 +42,24 @@ const allowedPets = 42;
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-
+let result = 'not conditional';
+if( adventurous == true ){
+  result = 'Adventures are great!';
+}
+else{
+  result = "How about we stay home?";
+}
+console.log(result);
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
-
+let diceRoll = 'Try again later.';
+if (luckyNumber == 2 && adventurous == true) {
+  diceRoll = 'Roll the dice!';
+}
+console.log(diceRoll);
 // 14 - Create a variable called `petStatus`. 
 //      Write a conditional that covers the following:
 //      if the value of `pets` is less than the value of `allowedPets`,
@@ -57,8 +68,18 @@ const allowedPets = 42;
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
+let petStatus = 'undefined';
+if (pets < allowedPets) {
+  petStatus = 'I can have more pets';
+}
+else if (pets == allowedPets) {
+  petStatus = 'I have enough pets';
+}
+else if (pets > allowedPets) {
+  petStatus = 'Oh no, I have too many pets!';
+}
 
-
+console.log(petStatus);
 // STRETCH GOALS:
 
 // 15 - Make a variable called `mostPets` and a conditional that
