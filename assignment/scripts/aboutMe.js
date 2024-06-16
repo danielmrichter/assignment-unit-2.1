f1ba7f327a3a427c1a373fceb6c415760bb783ed
Daniel Richter <drichter3697@gmail.com>
2024-06-16 15:21:23 -0500

    fixed a bug with ternary operator

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -124,7 +124,7 @@ console.log(luckyResult);
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
 
 
-result = (adventurous = true) ? 'Adventures are great!' : 'How about we stay home?';
+result = adventurous === true ? 'Adventures are great!' : 'How about we stay home?';
 
 console.log(result);
 
